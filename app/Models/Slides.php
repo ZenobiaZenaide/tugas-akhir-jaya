d37e0758aac23d5341a4407ee1f55a2c734878f8
gamer <sodiumcation@gmail.com>
2025-05-06 18:58:46 +0700

    gamer

--- app/Models/Slides.php
+++ app/Models/Slides.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Concerns\HasUuids; // Add this import
 use Illuminate\Database\Eloquent\Model;
 
 class Slides extends Model // Or class Slide extends Model if that's the actual class name
 {
-    use HasUuids; // Add this trait
-
-    
+    // Use string as primary key
+    protected $keyType = 'string';
+    // Not auto-incrementing
+    public $incrementing = false;    
 }
